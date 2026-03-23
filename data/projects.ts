8f43c2f86135a1940c0e548b0d2755c59225e4ac
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-03-23 19:29:51 +0530

    Updated

--- data/projects.ts
+++ data/projects.ts
@@ -20,7 +20,7 @@ export const projects: Project[] = [
     description:
       'A Visual Studio Code themed developer portfolio built with Next.js and CSS Modules.',
     logo: '/logos/vsc.svg',
-    link: 'https://github.com/itsnitinr/vscode-portfolio',
+    link: 'https://github.com/ermadhav/ISS_Tracker',
     slug: 'vscode-portfolio',
   },
   {
